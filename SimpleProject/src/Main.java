a9f8917cd0e8a2da17458033a77946d7619fd162
hp6560 <nabil1984@gmail.com>
2014-05-18 18:52:39 +0100

    premiere verification de saletools

---
+++ SimpleProject/src/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data=new SalesData();
+		displayGreeting();
+		data.display();
+
+	}
+	
+	private static void displayGreeting()
+	{
+		System.out.println("affichage 1");
+		System.out.println("affichage 2");
+	}
+
+}
---
+++ SimpleProject/src/SalesData.java
@@ -0,0 +1,9 @@
+
+public class SalesData {
+	int data[]={1,2,3};
+	public void display()
+	{
+		System.out.println("affichage de data");
+	}
+
+}
